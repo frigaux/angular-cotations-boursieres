c3488a2747e163541f996312d54ca489100b4dd6
fabien <fabien.rigaux@free.fr>
2025-07-03 19:17:48 +0200

    footer.component.html : affichage d'informations concernant le user agent et la résolution d'affichage

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.sass'
 })
 export class FooterComponent {
+  navigator: Navigator;
+  screen: Screen;
 
+  constructor() {
+    this.screen = window.screen;
+    this.navigator = window.navigator;
+  }
 }
